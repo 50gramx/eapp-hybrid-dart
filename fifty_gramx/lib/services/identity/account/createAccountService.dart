956792ed19fe3cd4e6487d798a2b087a0aa6752d
cs145442 <amitkumarkhetan15@gmail.com>
2021-05-21 16:49:23 +0530

    added all the needful createAccountService.dart cloud functions

--- fifty_gramx/lib/services/identity/account/createAccountService.dart
+++ fifty_gramx/lib/services/identity/account/createAccountService.dart
@@ -1,11 +1,13 @@
 import 'package:fifty_gramx/common/identityCommonChannel.dart';
 import 'package:fifty_gramx/protos/ethos/elint/entities/account.pb.dart';
+import 'package:fifty_gramx/protos/ethos/elint/entities/generic.pb.dart';
 import 'package:fifty_gramx/protos/ethos/elint/services/product/identity/account/create_account.pb.dart';
 import 'package:fifty_gramx/protos/ethos/elint/services/product/identity/account/create_account.pbgrpc.dart';
 
 class CreateAccountService {
   static Future<ValidateAccountWithMobileResponse> validateAccountWithMobile(
-      String accountMobileNumber) async {
+    String accountMobileNumber,
+  ) async {
     var createAccountServiceClient =
         CreateAccountServiceClient(await IdentityCommonChannel.identityChannel);
     var request = ValidateAccountWithMobileRequest()
@@ -13,5 +15,45 @@ class CreateAccountService {
       ..accountMobileNumber = accountMobileNumber
       ..requestedAt = Timestamp();
     return await createAccountServiceClient.validateAccountWithMobile(request);
+  }
+
+  static Future<VerificationAccountResponse> verificationAccount(
+    AccountCreationAuthDetails accountCreationAuthDetails,
+    bool resendCode,
+    String verificationCode,
+    TemporaryTokenDetails verificationCodeTokenDetails,
+  ) async {
+    var createAccountServiceClient =
+        CreateAccountServiceClient(await IdentityCommonChannel.identityChannel);
+    var request = VerificationAccountRequest()
+      ..accountCreationAuthDetails = accountCreationAuthDetails
+      ..resendCode = resendCode
+      ..verificationCode = verificationCode
+      ..verificationCodeTokenDetails = verificationCodeTokenDetails
+      ..requestedAt = Timestamp();
+    return await createAccountServiceClient.verificationAccount(request);
+  }
+
+  static Future<CaptureAccountMetaDetailsResponse> captureAccountMetaDetails(
+    AccountCreationAuthDetails accountCreationAuthDetails,
+    String accountFirstName,
+    String accountLastName,
+    Timestamp accountBirthAt,
+    AccountGender accountGender,
+    AccountDeviceDetails accountDeviceDetails,
+    String accountAssistantName,
+  ) async {
+    var createAccountServiceClient =
+        CreateAccountServiceClient(await IdentityCommonChannel.identityChannel);
+    var request = CaptureAccountMetaDetailsRequest()
+      ..accountCreationAuthDetails = accountCreationAuthDetails
+      ..accountFirstName = accountFirstName
+      ..accountLastName = accountLastName
+      ..accountBirthAt = accountBirthAt
+      ..accountGender = accountGender
+      ..accountDeviceDetails = accountDeviceDetails
+      ..accountAssistantName = accountAssistantName
+      ..requestedAt = Timestamp();
+    return await createAccountServiceClient.captureAccountMetaDetails(request);
   }
 }
