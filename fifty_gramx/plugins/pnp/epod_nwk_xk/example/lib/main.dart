c9ec77cbc6076ce5d71e59851ddfae06e641110d
amit.khetan.70 <yn2FdDtFWDbjfTQpCcqV>
2022-08-17 23:44:05 +0530

    refactored all plugins path in app

--- fifty_gramx/plugins/pnp/epod_nwk_xk/example/lib/main.dart
+++ fifty_gramx/plugins/pnp/epod_nwk_xk/example/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:epod_nwk_xk/epod_nwk_xk.dart';
 
 void main() {
   runApp(const MyApp());
