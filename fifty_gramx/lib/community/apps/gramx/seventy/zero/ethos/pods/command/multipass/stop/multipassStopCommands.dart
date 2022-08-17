b2d6c5bfbf6e8a3c69b7d44a31ec79369d809278
amit.khetan.70 <yn2FdDtFWDbjfTQpCcqV>
2022-08-18 00:55:54 +0530

    added multipass stop commands

---
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/multipass/stop/multipassStopCommands.dart
@@ -0,0 +1,35 @@
+// operates the stop commands for multipass package
+//
+// needs [String] packagePath and [String] vmName as initializers
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/executer/privilegedCommandExecuter.dart';
+
+class MultipassStopCommands {
+  MultipassStopCommands._();
+
+  /// contains the package path
+  static String _packagePath = "";
+
+  /// contains the virtual machine name
+  static String _vmName = "";
+
+  /// contains the base command for launch commands
+  static String _baseCommandSpace = "";
+
+  // initialises the class
+  MultipassStopCommands(String packagePath, String vmName) {
+    _packagePath = packagePath;
+    _vmName = vmName;
+    _baseCommandSpace = "${_packagePath} stop ";
+  }
+
+  /// starts the orchestrator
+  ///
+  /// returns the status codes
+  orchestrator() async {
+    // build the command
+    String command = "${_baseCommandSpace}"
+        "${_vmName}";
+    // run the command
+    await PrivilegedCommandExecuter.run(command);
+  }
+}
