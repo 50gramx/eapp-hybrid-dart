8914e2816ece8f90b06e76d00aa2bd7782863416
amit.khetan.70 <yn2FdDtFWDbjfTQpCcqV>
2022-08-18 00:28:22 +0530

    moved and updated package paths

---
+++ fifty_gramx/lib/community/apps/gramx/fifty/five/ethos/eutopia/components/IconButton/Entity/AccountAssistantIconButton.dart
@@ -0,0 +1,84 @@
+import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/eutopia/colors/AppColors.dart';
+import 'package:fifty_gramx/protos/ethos/elint/entities/account_assistant.pb.dart';
+import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/eutopia/components/IconButton/ConnectedEntityIconButton.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+class AccountAssistantIconButton implements ConnectedEntityIconButton {
+  final AccountAssistant connectedAccountAssistant;
+
+  AccountAssistantIconButton(this.connectedAccountAssistant);
+
+  @override
+  Widget buildConnectedContactIconButton(BuildContext context) => SizedBox();
+
+  @override
+  Widget buildConnectedAccountIconButton(BuildContext context) => SizedBox();
+
+  @override
+  Widget buildConnectedAccountAssistantIconButton(BuildContext context) {
+    return Neumorphic(
+      style: NeumorphicStyle(
+        color: AppColors.backgroundPrimary(context),
+        lightSource: NeumorphicTheme.isUsingDark(context)
+            ? LightSource.bottomRight
+            : LightSource.topLeft,
+        shadowLightColor: NeumorphicTheme.isUsingDark(context)
+            ? AppColors.gray600
+            : AppColors.backgroundSecondary(context),
+        border: NeumorphicBorder(
+            color: AppColors.backgroundPrimary(context), width: 0.5),
+        boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(24)),
+      ),
+      padding: const EdgeInsets.all(1),
+      child: NeumorphicButton(
+        provideHapticFeedback: true,
+         onPressed: () {},
+        style: NeumorphicStyle(
+          lightSource: NeumorphicTheme.isUsingDark(context)
+              ? LightSource.bottomRight
+              : LightSource.topLeft,
+          shadowLightColor: NeumorphicTheme.isUsingDark(context)
+              ? AppColors.gray600
+              : AppColors.backgroundSecondary(context),
+          shape: NeumorphicShape.convex,
+          boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(24)),
+          color: AppColors.backgroundPrimary(context),
+        ),
+        padding: const EdgeInsets.all(2),
+        child: CircleAvatar(
+          radius: 20,
+          backgroundColor: AppColors.backgroundPrimary(context),
+          child: NeumorphicText(
+            "${connectedAccountAssistant.accountAssistantNameCode < 10 ? "0${connectedAccountAssistant.accountAssistantNameCode}" : "${connectedAccountAssistant.accountAssistantNameCode}"}",
+            style: NeumorphicStyle(
+              color: AppColors.contentPrimary(context),
+              lightSource: NeumorphicTheme.isUsingDark(context)
+                  ? LightSource.bottomRight
+                  : LightSource.topLeft,
+              shadowLightColor: NeumorphicTheme.isUsingDark(context)
+                  ? AppColors.gray600
+                  : AppColors.backgroundSecondary(context),
+              border: NeumorphicBorder(
+                  color: AppColors.backgroundPrimary(context), width: 0.5),
+            ),
+            textAlign: TextAlign.start,
+            textStyle: NeumorphicTextStyle(
+                fontFamily: "Montserrat",
+                fontSize: 18,
+                fontWeight: FontWeight.w800,
+                height: 1.25),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget buildConnectedAccountMetaIconButton(BuildContext context) =>
+      SizedBox();
+
+  @override
+  Widget buildConnectedOrganisationIconButton(BuildContext context) =>
+      SizedBox();
+}
---
+++ fifty_gramx/lib/community/apps/gramx/fifty/five/ethos/eutopia/components/IconButton/Spaces/KnowledgeIconButton.dart
@@ -0,0 +1,30 @@
+import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/eutopia/colors/AppColors.dart';
+import 'package:fifty_gramx/protos/ethos/elint/entities/space_knowledge_domain.pb.dart';
+import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/eutopia/components/IconButton/DomainSpaceIconButton.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+
+class KnowledgeIconButton implements DomainSpaceIconButton {
+  final SpaceKnowledgeDomain spaceKnowledgeDomain;
+
+  KnowledgeIconButton(this.spaceKnowledgeDomain);
+
+  @override
+  Widget buildKnowledgeDomainSpaceIconButton(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        // TODO: Implement Space Admin Picture with this icon
+        FeatherIcons.user,
+        size: 16,
+      ),
+      color: AppColors.lightNeuSecondary,
+      onPressed: () {},
+    );
+  }
+
+  @override
+  Widget buildProductDomainSpaceIconButton(BuildContext context) => SizedBox();
+
+  @override
+  Widget buildServiceDomainSpaceIconButton(BuildContext context) => SizedBox();
+}
