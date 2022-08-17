82cc351de0dd66843c89b3b55dd463f27725e195
amit.khetan.70 <yn2FdDtFWDbjfTQpCcqV>
2022-08-18 00:15:44 +0530

    added brew install and commands to manage brew commands on macos

---
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/brew/brewCommands.dart
@@ -0,0 +1,107 @@
+import 'dart:convert';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/brew/install/brewInstallCommands.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/executer/privilegedCommandExecuter.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/executer/simpleCommandExecuter.dart';
+import 'package:fifty_gramx/data/hostUserData.dart';
+import 'package:process_run/shell.dart';
+
+// operates the commands for brew package
+//
+// needs no initializers
+// it is needed to initialise brew during app startup
+class BrewCommands {
+  // singleton definition
+  BrewCommands._();
+
+  /// contains the package name as per the base os
+  static String _packageName = "";
+
+  /// contains the package path as per the base os
+  static String _packagePath = "";
+
+  /// nested class for install commands
+  static late BrewInstallCommands install;
+
+  // initialises the class
+  // warn: initBrew initialised the class during startup, if defined
+  BrewCommands() {}
+
+  static initBrew() async {
+    // package name is same in all base os, so no checks needed
+    _packageName = "brew";
+
+    // package path is defined as per the os
+    // since the path of the package, depends on the device architecture
+    // we need to get the arch and set the package path respectively
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    var macOsInfo = await deviceInfo.macOsInfo;
+    if (macOsInfo.arch == "arm64") {
+      _packagePath = "/opt/homebrew/bin/$_packageName";
+    } else {
+      _packagePath = "/opt/local/bin/$_packageName";
+    } // TODO(founder@): add a check for MacOS
+
+    install = BrewInstallCommands(_packagePath);
+  }
+
+  static Future<String> setupBrew() async {
+    // Before setting up lets check if available on host machine or not,
+    // If available, we'll skip setup
+    var preSetupVersion = await BrewCommands.version();
+    if (preSetupVersion != "NA") {
+      return "Already available, skipping the setup";
+    }
+    var assetPath =
+        await PrivilegedCommandExecuter.getTemporaryFilePathForAssetFile(
+            "lib/community/apps/gramx/seventy/zero/ethos/pods/configurations/orchestrator/setup/scripts/homebrew.sh");
+    var installPath =
+        await PrivilegedCommandExecuter.getTemporaryFilePathForAssetFile(
+            "lib/community/apps/gramx/seventy/zero/ethos/pods/configurations/orchestrator/setup/scripts/install.sh");
+    // get the host user password
+    String password = await HostUserData().readHostUserPassword();
+    // Build the spawn command
+    var spawnCommand = "spawn /bin/bash $assetPath";
+    // Update the spawn command in install script
+    await SimpleCommandExecuter.run(
+        '''sed -e '4s#.*#$spawnCommand#' -i '' $installPath''');
+    // Update password in install script
+    await SimpleCommandExecuter.run(
+        '''sed -e '6s/.*/send "$password"/' -i '' $installPath''');
+    // Run the install script
+    await SimpleCommandExecuter.run("/bin/bash $installPath");
+    // Checking if brew is successfully installed
+    var postSetupVersion = await BrewCommands.version();
+    if (postSetupVersion == "NA") {
+      return "Something went wrong";
+    } else {
+      // since the homebrew setup is done
+      // let's also install cask needed for multipass
+      await SimpleCommandExecuter.run("$_packagePath install cask");
+      return postSetupVersion;
+    }
+  }
+
+  static Future<String> version() async {
+    // returns the version of brew if available
+    // else, returns NA
+    var version = await SimpleCommandExecuter.run("$_packagePath -v");
+    if (version.outText.length > 0) {
+      if (version.outText.contains("Homebrew")) {
+        return LineSplitter.split(version.outText).first.substring(9);
+      } else {
+        return "NA";
+      }
+    } else {
+      return "NA";
+    }
+  }
+}
+
+// TODO: add below command and their executors
+/**
+ * casks
+ * search
+ * cleanup
+ * */
---
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/brew/install/brewInstallCommands.dart
@@ -0,0 +1,49 @@
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/executer/privilegedCommandExecuter.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/executer/simpleCommandExecuter.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/microk8s/microk8sCommands.dart';
+import 'package:fifty_gramx/data/hostUserData.dart';
+
+class BrewInstallCommands {
+  /// contains the package path
+  static String _packagePath = "";
+
+  /// contains the base command for install commands
+  static String _baseCommandSpace = "";
+
+  /// nested class for microk8s commands
+  static late Microk8sCommands microk8s;
+
+  // initialises the class
+  BrewInstallCommands(String packagePath) {
+    // package path comes from the parent, so no checks needed
+    _packagePath = packagePath;
+
+    // base command is same for all os, so no checks needed
+    _baseCommandSpace = "${_packagePath} install ";
+  }
+
+  multipass() async {
+    // build the command
+    String command = "${_baseCommandSpace}"
+        "--cask multipass";
+    // get the host user password
+    String password = await HostUserData().readHostUserPassword();
+    String assetPath =
+        "lib/community/apps/gramx/seventy/zero/ethos/pods/configurations/orchestrator/setup/scripts/installMultipass.sh";
+    var installPath =
+        await PrivilegedCommandExecuter.getTemporaryFilePathForAssetFile(
+            assetPath);
+    // Update the spawn command in install script
+    await SimpleCommandExecuter.run(
+        '''sed -e '4s#.*#spawn $command#' -i '' $installPath''');
+    // Update password in install script
+    await SimpleCommandExecuter.run(
+        '''sed -e '6s/.*/send "$password"/' -i '' $installPath''');
+    // Run the install script
+    print("will run installer");
+    await SimpleCommandExecuter.run("/bin/bash $installPath");
+    // Wait for the socket to up and running
+    print("will start waiting for 10sec");
+    await Future.delayed(Duration(seconds: 10));
+  }
+}
