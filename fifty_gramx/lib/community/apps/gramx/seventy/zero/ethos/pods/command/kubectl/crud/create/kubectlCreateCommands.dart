75a6d5ed01a11ee06d3bc0e817a19733af22cfda
amit.khetan.70 <yn2FdDtFWDbjfTQpCcqV>
2022-08-21 08:27:38 +0530

    added nested class for namespaced resources

--- fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/crud/create/kubectlCreateCommands.dart
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/crud/create/kubectlCreateCommands.dart
@@ -1,6 +1,7 @@
 // operates the create commands for kubectl package
 
 import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/executer/simpleCommandExecuter.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/kubectlNamespacedResources.dart';
 
 class KubectlCreateCommands {
   KubectlCreateCommands._();
@@ -11,12 +12,17 @@ class KubectlCreateCommands {
   /// contains the base command for create commands
   static String _baseCommandSpace = "";
 
+  /// nested class for create commands
+  KubectlNamespacedResources get namespaced =>
+      KubectlNamespacedResources(_baseCommandSpace);
+
   // initialises the class
   KubectlCreateCommands(String packagePath) {
     _packagePath = packagePath;
     _baseCommandSpace = "${_packagePath}create ";
   }
 
+  // TODO: We need to deprecate this
   /// creates the resource from configuration file
   ///
   /// needs [String] configuration file path
