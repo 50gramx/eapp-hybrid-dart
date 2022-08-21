ee4bc0c22404db7e920689676ef9cc806f842aca
amit.khetan.70 <yn2FdDtFWDbjfTQpCcqV>
2022-08-21 08:25:03 +0530

    added multipass transfer commands

---
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/multipass/transfer/multipassTransferCommands.dart
@@ -0,0 +1,47 @@
+// operates the transfer commands for multipass package
+//
+// needs [String] packagePath and [String] vmName as initializers
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/executer/privilegedCommandExecuter.dart';
+
+class MultipassTransferCommands {
+  MultipassTransferCommands._();
+
+  /// contains the package path
+  static String _packagePath = "";
+
+  /// contains the virtual machine name
+  static String _vmName = "";
+
+  /// contains the base command for launch commands
+  static String _baseCommandSpace = "";
+
+  /// contains the base path for transfer commands
+  static String _vmBasePath = "/home/ubuntu";
+
+  // initialises the class
+  MultipassTransferCommands(String packagePath, String vmName) {
+    _packagePath = packagePath;
+    _vmName = vmName;
+    _baseCommandSpace = "${_packagePath} transfer ";
+  }
+
+  /// transfers the asset from host machine to vm
+  ///
+  /// returns the vm asset path
+  /// which can be generally used to delete asset after use from vm
+  Future<String> asset(String assetPath) async {
+    // get the temporary asset path in host machine
+    String temporaryAssetPath =
+        await PrivilegedCommandExecuter.getTemporaryFilePathForAssetFile(
+            assetPath);
+    // build the command
+    String command = "${_baseCommandSpace}"
+        "$temporaryAssetPath ${_vmName}:.";
+    // run the command
+    await PrivilegedCommandExecuter.run(command);
+    // build the asset path in vm
+    String vmAssetPath = "${_vmBasePath}/${temporaryAssetPath.split("/").last}";
+    // return the vm asset path
+    return vmAssetPath;
+  }
+}
