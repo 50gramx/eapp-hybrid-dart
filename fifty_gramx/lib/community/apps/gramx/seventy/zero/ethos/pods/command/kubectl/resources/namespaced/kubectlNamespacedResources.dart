aa41a3a9f2a75d43cea1b95c632c9e8c0ff42629
amit.khetan.70 <yn2FdDtFWDbjfTQpCcqV>
2022-08-24 08:11:42 +0530

    added nested class for sts, deploy and svc

--- fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/kubectlNamespacedResources.dart
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/kubectlNamespacedResources.dart
@@ -1,11 +1,23 @@
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/deployment/kubectlNamespacedDeploymentResources.dart';
 import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/namespace/kubectlNamespacedNamespaceResources.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/service/kubectlNamespacedServiceResources.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/statefulset/kubectlNamespacedStatefulsetResources.dart';
 
 class KubectlNamespacedResources {
   /// contains the base command for namespaced resources
   static String _baseCommandSpace = "";
 
-  /// nested class for namespaced resources
+  /// nested class for namespace resources
   late KubectlNamespacedNamespaceResources namespace;
+
+  /// nested class for statefulset resources
+  late KubectlNamespacedStatefulsetResources statefulset;
+
+  /// nested class for deployment resources
+  late KubectlNamespacedDeploymentResources deployment;
+
+  /// nested class for service resources
+  late KubectlNamespacedServiceResources service;
 
   // initialises the class
   KubectlNamespacedResources(String baseCommandSpace) {
@@ -13,5 +25,14 @@ class KubectlNamespacedResources {
 
     // initialize the namespace resource
     namespace = KubectlNamespacedNamespaceResources(_baseCommandSpace);
+
+    // initialize the statefulset resource
+    statefulset = KubectlNamespacedStatefulsetResources(_baseCommandSpace);
+
+    // initialize the deployment resource
+    deployment = KubectlNamespacedDeploymentResources(_baseCommandSpace);
+
+    // initialize the service resource
+    service = KubectlNamespacedServiceResources(_baseCommandSpace);
   }
 }
