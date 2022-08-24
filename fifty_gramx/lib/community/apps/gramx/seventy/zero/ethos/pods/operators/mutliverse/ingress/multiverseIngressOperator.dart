33d926a15d0b108cd9984e0d3bee0a4203dd4be6
amit.khetan.70 <yn2FdDtFWDbjfTQpCcqV>
2022-08-24 12:58:19 +0530

    updated steps to spinUp multiverse ingress

--- fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/operators/mutliverse/ingress/multiverseIngressOperator.dart
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/operators/mutliverse/ingress/multiverseIngressOperator.dart
@@ -40,23 +40,58 @@ class MultiverseIngressOperator {
     // check if all the containers in the pod is not running
     if (!isUp()) {
       // steps to spin up ingress
+
       // create admissions service account
+      // await KubectlCommands.apply.namespaced.serviceAccount.multiverseIngressAdmissions();
+
       // create admissions role
+      // await KubectlCommands.apply.namespaced.role.multiverseIngressAdmissions();
+
       // create admissions role bindings
+      // await KubectlCommands.apply.namespaced.roleBinding.multiverseIngressAdmissions();
+
       // create admissions cluster role
+      // await KubectlCommands.apply.cluster.clusterRole.multiverseIngressAdmissions();
+
       // create admissions cluster role bindings
+      // await KubectlCommands.apply.cluster.clusterRoleBinding.multiverseIngressAdmissions();
+
       // create admissions validate webhook
+      // await KubectlCommands.apply.namespaced.configuration.multiverseIngressAdmissionsValidateWebhook();
+
       // create admissions create job
+      // await KubectlCommands.apply.namespaced.job.multiverseIngressAdmissionsCreate();
+
       // create admissions patch job
+      // await KubectlCommands.apply.namespaced.job.multiverseIngressAdmissionsPatch();
+
       // create controller service account
+      // await KubectlCommands.apply.namespaced.serviceAccount.multiverseIngressController();
+
       // create controller role
+      // await KubectlCommands.apply.namespaced.role.multiverseIngressController();
+
       // create controller role bindings
+      // await KubectlCommands.apply.namespaced.roleBinding.multiverseIngressController();
+
       // create controller cluster role
+      // await KubectlCommands.apply.cluster.clusterRole.multiverseIngressController();
+
       // create controller cluster role bindings
+      // await KubectlCommands.apply.cluster.clusterRoleBinding.multiverseIngressController();
+
       // create controller config map
+      // await KubectlCommands.apply.namespaced.configMap.multiverseIngressController();
+
       // create controller service
+      // await KubectlCommands.apply.namespaced.service.multiverseIngressController();
+
       // create controller admission service
+      // await KubectlCommands.apply.namespaced.service.multiverseIngressAdmission();
+
       // create controller deployment
+      // await KubectlCommands.apply.namespaced.deployment.multiverseIngressController();
+
       // todo: create ingress resource for services
       await KubectlCommands.apply.namespaced.deployment
           .multiverseChainsIdentity();
