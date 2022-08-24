1d9c9cad3961be287d519139c5c5295fe73a81ab
amit.khetan.70 <yn2FdDtFWDbjfTQpCcqV>
2022-08-24 08:08:44 +0530

    added multiverse chain operator nested class

---
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/operators/mutliverse/multiversePodOperator.dart
@@ -0,0 +1,10 @@
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/operators/mutliverse/chains/multiverseChainsOperator.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/operators/mutliverse/filesystem/multiverseFileSystemOperator.dart';
+
+class MultiversePodOperator {
+  // operates the multiverse pods
+  static Map<String, dynamic> _multiversePodDetails = {};
+
+  static MultiverseFileSystemOperator fsOp = MultiverseFileSystemOperator();
+  static MultiverseChainsOperator xcOp = MultiverseChainsOperator();
+}
