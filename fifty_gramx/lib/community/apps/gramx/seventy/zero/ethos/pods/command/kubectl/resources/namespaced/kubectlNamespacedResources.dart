d023bc1e5a2cfef734d0c7e6dd2f44cdf41b78b8
amit.khetan.70 <yn2FdDtFWDbjfTQpCcqV>
2022-08-26 23:15:13 +0530

    added nested class for role, roleBinding, configuration, job, configMap, ingress

--- fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/kubectlNamespacedResources.dart
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/kubectlNamespacedResources.dart
@@ -1,5 +1,11 @@
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/configMap/kubectlNamespacedConfigMapResources.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/configuration/kubectlNamespacedConfigurationResources.dart';
 import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/deployment/kubectlNamespacedDeploymentResources.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/ingress/kubectlNamespacedIngressResources.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/job/kubectlNamespacedJobResources.dart';
 import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/namespace/kubectlNamespacedNamespaceResources.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/role/kubectlNamespacedRoleResources.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/roleBinding/kubectlNamespacedRoleBindingResources.dart';
 import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/service/kubectlNamespacedServiceResources.dart';
 import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/statefulset/kubectlNamespacedStatefulsetResources.dart';
 
@@ -19,6 +25,24 @@ class KubectlNamespacedResources {
   /// nested class for service resources
   late KubectlNamespacedServiceResources service;
 
+  /// nested class for role resources
+  late KubectlNamespacedRoleResources role;
+
+  /// nested class for role binding resources
+  late KubectlNamespacedRoleBindingResources roleBinding;
+
+  /// nested class for configuration resources
+  late KubectlNamespacedConfigurationResources configuration;
+
+  /// nested class for job resources
+  late KubectlNamespacedJobResources job;
+
+  /// nested class for job resources
+  late KubectlNamespacedConfigMapResources configMap;
+
+  /// nested class for ingress resources
+  late KubectlNamespacedIngressResources ingress;
+
   // initialises the class
   KubectlNamespacedResources(String baseCommandSpace) {
     _baseCommandSpace = baseCommandSpace;
@@ -34,5 +58,23 @@ class KubectlNamespacedResources {
 
     // initialize the service resource
     service = KubectlNamespacedServiceResources(_baseCommandSpace);
+
+    // initialize the role resource
+    role = KubectlNamespacedRoleResources(_baseCommandSpace);
+
+    // initialize the role binding resource
+    roleBinding = KubectlNamespacedRoleBindingResources(_baseCommandSpace);
+
+    // initialize the configuration resource
+    configuration = KubectlNamespacedConfigurationResources(_baseCommandSpace);
+
+    // initialize the job resource
+    job = KubectlNamespacedJobResources(_baseCommandSpace);
+
+    // initialize the configmap resource
+    configMap = KubectlNamespacedConfigMapResources(_baseCommandSpace);
+
+    // initialize the ingress resource
+    ingress = KubectlNamespacedIngressResources(_baseCommandSpace);
   }
 }
