fd57e7f8ed67070e659ebc9239a8cb8c3ebc1cf3
amit.khetan.70 <yn2FdDtFWDbjfTQpCcqV>
2022-08-26 23:00:28 +0530

    added nested classes for cluster role and cluster role binding in k8s cluster resource manager

--- fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/cluster/kubectlClusterResources.dart
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/cluster/kubectlClusterResources.dart
@@ -1,3 +1,5 @@
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/cluster/clusterRole/kubectlClusterClusterRoleResources.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/cluster/clusterRoleBinding/kubectlClusterClusterRoleBindingResources.dart';
 import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/cluster/persistentVolume/kubectlClusterPersistentVolumeResources.dart';
 
 class KubectlClusterResources {
@@ -7,6 +9,12 @@ class KubectlClusterResources {
   /// nested class for namespace resources
   late KubectlClusterPersistentVolumeResources persistentVolume;
 
+  /// nested class for namespace resources
+  late KubectlClusterClusterRoleResources clusterRole;
+
+  /// nested class for namespace resources
+  late KubectlClusterClusterRoleBindingResources clusterRoleBinding;
+
   // initialises the class
   KubectlClusterResources(String baseCommandSpace) {
     _baseCommandSpace = baseCommandSpace;
@@ -14,5 +22,12 @@ class KubectlClusterResources {
     // initialize the namespace resource
     persistentVolume =
         KubectlClusterPersistentVolumeResources(_baseCommandSpace);
+
+    // initialize the namespace resource
+    clusterRole = KubectlClusterClusterRoleResources(_baseCommandSpace);
+
+    // initialize the namespace resource
+    clusterRoleBinding =
+        KubectlClusterClusterRoleBindingResources(_baseCommandSpace);
   }
 }
