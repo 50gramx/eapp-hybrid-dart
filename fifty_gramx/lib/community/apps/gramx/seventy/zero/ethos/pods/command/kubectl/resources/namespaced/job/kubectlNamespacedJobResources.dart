dc2c0c7bd3133d94d40fd75706fd207225aec91d
amit.khetan.70 <yn2FdDtFWDbjfTQpCcqV>
2022-08-26 23:07:33 +0530

    added job resource manager for multiverse-service-ingress admissions create and patch

---
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/kubectl/resources/namespaced/job/kubectlNamespacedJobResources.dart
@@ -0,0 +1,50 @@
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/executer/simpleCommandExecuter.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/multipass/multipassCommands.dart';
+
+class KubectlNamespacedJobResources {
+  /// contains the base command for resource management commands
+  static String _baseCommandSpace = "";
+
+  // initialises the class
+  KubectlNamespacedJobResources(String baseCommandSpace) {
+    _baseCommandSpace = baseCommandSpace;
+  }
+
+  /// creates the resource for "multiverse-ingress-admission-create"
+  ///
+  /// warning, it is intended to work only
+  /// when nested under kubectl apply commands
+  Future<bool> multiverseIngressAdmissionCreate() async {
+    // fetch the temporary path for configuration file from the assets
+    String assetPath =
+        "lib/community/apps/gramx/seventy/zero/ethos/pods/configurations/multiverse/resources/namespaced/ingress-nginx-admission-create-job.yaml";
+    // transfer the asset to vm
+    String vmAssetPath = await MultipassCommands.transfer.asset(assetPath);
+    // build the command
+    String command = "$_baseCommandSpace -f $vmAssetPath";
+    // run the command
+    await SimpleCommandExecuter.run(command);
+    // remove the asset from vm
+    await MultipassCommands.exec.removeAsset(vmAssetPath);
+    return true;
+  }
+
+  /// creates the resource for "multiverse-ingress-admission-patch"
+  ///
+  /// warning, it is intended to work only
+  /// when nested under kubectl apply commands
+  Future<bool> multiverseIngressAdmissionPatch() async {
+    // fetch the temporary path for configuration file from the assets
+    String assetPath =
+        "lib/community/apps/gramx/seventy/zero/ethos/pods/configurations/multiverse/resources/namespaced/ingress-nginx-admission-patch-job.yaml";
+    // transfer the asset to vm
+    String vmAssetPath = await MultipassCommands.transfer.asset(assetPath);
+    // build the command
+    String command = "$_baseCommandSpace -f $vmAssetPath";
+    // run the command
+    await SimpleCommandExecuter.run(command);
+    // remove the asset from vm
+    await MultipassCommands.exec.removeAsset(vmAssetPath);
+    return true;
+  }
+}
