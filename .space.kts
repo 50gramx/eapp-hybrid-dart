5c6a5386f74f16583489b5f405a34d7bb9b2c3b9
Amit Khetan <amit.khetan.70@50gramx.io>
2023-07-14 22:43:00 +0000

    updated docker build

--- .space.kts
+++ .space.kts
@@ -8,7 +8,7 @@ job("Build Base Image") {
     startOn {
         gitPush {
             pathFilter {
-                +"Dockerfile.base"
+                +"Dockerfile.web.base"
             }
         }
     }
@@ -20,14 +20,14 @@ job("Build Base Image") {
             // push = false
 
             // path to Docker context (by default, context is working dir)
-            context = "docker"
+            // context = "docker"
             // path to Dockerfile relative to the project root
             // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
-            file = "docker/config/Dockerfile"
+            file = "Dockerfile.web.base"
             // build-time variables
-            args["HTTP_PROXY"] = "http://10.20.30.2:1234"
+            // args["HTTP_PROXY"] = "http://10.20.30.2:1234"
             // image labels
-            labels["vendor"] = "mycompany"
+            // labels["vendor"] = "mycompany"
             // to add a raw list of additional build arguments, use
             // extraArgsForBuildCommand = listOf("...")
             // to add a raw list of additional push arguments, use
@@ -35,7 +35,7 @@ job("Build Base Image") {
             // image tags
             tags {
                 // use current job run number as a tag - '0.0.run_number'
-                +"mycompany.registry.jetbrains.space/p/prjkey/mydocker/myimage:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+                +"50gramx.registry.jetbrains.space/p/main/ethosindiacontainers/web-base:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
             }
         }
     }
@@ -43,9 +43,16 @@ job("Build Base Image") {
 
 job("web release") {	
   
-    container(displayName = "Build Web Release", image = "ubuntu") {
+    container(displayName = "Build Web Release", image = "50gramx.registry.jetbrains.space/p/main/ethosindiacontainers/web-base") {
     	shellScript {
           content = """
+          	flutter doctor -v
+          """
+        }
+    }
+}
+
+/* 
           	echo "update ubuntu"
             apt update -y
             echo "install flutter CLI Tools"
@@ -69,7 +76,4 @@ job("web release") {
             flutter doctor -v
             echo "build web release"
            	cd fifty_gramx && flutter build web --release
-          """
-        }
-    }
-}
+*/
