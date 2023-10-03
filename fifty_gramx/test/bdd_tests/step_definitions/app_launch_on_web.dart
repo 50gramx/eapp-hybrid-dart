43371a267db0e8005934feb493d04d6a390e47c2
amit.khetan.70 <amit.khetan.70@50gramx.io>
2023-10-03 06:54:00 +0530

    added step definition

---
+++ fifty_gramx/test/bdd_tests/step_definitions/app_launch_on_web.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'package:gherkin/gherkin.dart';
+
+StepDefinitionGeneric<FlutterWorld> givenUserHasAppInstalled() {
+  return given(
+    'the user has the 50gramx app installed',
+        (world) async {
+      print("givenUserHasAppInstalled");
+      // Add code here to simulate the app installation if needed
+    },
+  );
+}
+
+StepDefinitionGeneric<FlutterWorld> whenUserLaunchesAppOnWeb() {
+  return when(
+    'the user launches the app on the web',
+        (world) async {
+      // Add code here to simulate launching the app on the web
+    },
+  );
+}
+
+StepDefinitionGeneric<FlutterWorld> thenAppHomeScreen IsDisplayed() {
+  return then(
+    'the app\'s home screen should be displayed',
+        (world) async {
+      // Add code here to verify that the home screen is displayed
+      // You can use FlutterDriver to interact with the app and make assertions
+    },
+  );
+}
