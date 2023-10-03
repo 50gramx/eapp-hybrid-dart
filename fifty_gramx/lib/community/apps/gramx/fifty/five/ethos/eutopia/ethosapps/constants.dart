a789794eea3a1145da89b6c7efb868f0c2367122
amit.khetan.70 <amit.khetan.70@50gramx.io>
2023-10-03 06:50:08 +0530

    added constants.dart

---
+++ fifty_gramx/lib/community/apps/gramx/fifty/five/ethos/eutopia/ethosapps/constants.dart
@@ -0,0 +1,24 @@
+// File: constants.dart
+
+/// A map representing the textual representation of single digits.
+const Map<int, String> namedOnesMap = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+};
+
+/// A map representing the textual representation of tens digits.
+const Map<int, String> namedTenthsMap = {
+  50: 'fifty',
+  60: 'sixty',
+  70: 'seventy',
+  80: 'eighty',
+  90: 'ninety',
+};
