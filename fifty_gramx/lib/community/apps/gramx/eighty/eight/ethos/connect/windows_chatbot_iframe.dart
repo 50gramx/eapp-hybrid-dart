096116c493117156cd57da5a35b758e1bf15c706
amit.khetan.70 <amit.khetan.70@50gramx.io>
2023-11-10 03:36:24 +0530

    add universal chatbot frame

---
+++ fifty_gramx/lib/community/apps/gramx/eighty/eight/ethos/connect/windows_chatbot_iframe.dart
@@ -0,0 +1,9 @@
+import 'package:fifty_gramx/community/apps/gramx/eighty/eight/ethos/connect/base_chatbot_iframe.dart';
+import 'package:flutter/material.dart';
+
+class UniversalChatbotIframe extends BaseChatbotIframe {
+  @override
+  Widget buildIframe(String srcDoc) {
+    return SizedBox();
+  }
+}
