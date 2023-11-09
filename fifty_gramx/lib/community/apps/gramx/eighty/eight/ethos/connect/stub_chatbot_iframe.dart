2094966a9db79cbb866f7f376cb572626c84af0d
amit.khetan.70 <amit.khetan.70@50gramx.io>
2023-11-10 03:50:39 +0530

    add stub chatbot iframe

---
+++ fifty_gramx/lib/community/apps/gramx/eighty/eight/ethos/connect/stub_chatbot_iframe.dart
@@ -0,0 +1,8 @@
+import 'package:fifty_gramx/community/apps/gramx/eighty/eight/ethos/connect/base_chatbot_iframe.dart';
+import 'package:flutter/cupertino.dart';
+
+class UniversalChatbotIframe extends BaseChatbotIframe {
+  Widget buildIframe(String srcDoc) {
+    return SizedBox();
+  }
+}
