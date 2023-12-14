91507b9b3eca835801d6b3c373eb1d615adc5c68
amit.khetan.70 <amit.khetan.70@50gramx.io>
2023-12-15 02:56:13 +0530

    fix Dockerfile.android.base

--- .space.kts
+++ .space.kts
@@ -4,7 +4,7 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Build Base Image") {
+job("Build Web Base Image") {
     startOn {
         gitPush {
             pathFilter {
@@ -41,6 +41,44 @@ job("Build Base Image") {
     }
 }
 
+job("Build Android Base Image") {
+    startOn {
+        gitPush {
+            pathFilter {
+                +"Dockerfile.android.base"
+            }
+        }
+    }
+
+    host("Build and push android base image") {
+        dockerBuildPush {
+            // by default, the step runs not only 'docker build' but also 'docker push'
+            // to disable pushing, add the following line:
+            // push = false
+
+            // path to Docker context (by default, context is working dir)
+            // context = "docker"
+            // path to Dockerfile relative to the project root
+            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
+            file = "Dockerfile.android.base"
+            // build-time variables
+            // args["HTTP_PROXY"] = "http://10.20.30.2:1234"
+            // image labels
+            // labels["vendor"] = "mycompany"
+            // to add a raw list of additional build arguments, use
+            // extraArgsForBuildCommand = listOf("...")
+            // to add a raw list of additional push arguments, use
+            // extraArgsForPushCommand = listOf("...")
+            // image tags
+            tags {
+                // use current job run number as a tag - '0.0.run_number'
+                +"50gramx.registry.jetbrains.space/p/main/ethosindiacontainers/android-base:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+            }
+        }
+    }
+}
+
+
 job("web release") {
 	startOn {
         gitPush {
@@ -73,13 +111,41 @@ job("web release") {
             npm -v
            	npm install -g n 
             n stable
-          	cd fifty_gramx && flutter clean && flutter pub get && flutter pub cache repair && flutter build web --release && firebase deploy --token ${"$"}FIREBASE_TOKEN
+          	# cd fifty_gramx && flutter clean && flutter pub get && flutter pub cache repair && flutter build web --release && firebase deploy --token ${"$"}FIREBASE_TOKEN
           """
         }
 
     }
 }
 
+
+job("Build and publish bundle to internal track") {
+    // disable gitPush job trigger
+    startOn {
+        gitPush { enabled = false }
+    }
+
+    container("Build and publish", "mycompany.registry.jetbrains.space/p/projectkey/mydocker/automation-android:1.0.5") {
+        env["GOOGLE_SA_KEY"] = Secrets("google_sa_key")
+        env["KEY_STORE"] = Secrets("key_store")
+        env["KEY_STORE_PASSWORD"] = Secrets("key_store_password")
+        env["KEY_PASSWORD"] = Secrets("key_password")
+        env["KEY_ALIAS"] = Params("key_alias")
+
+        shellScript {
+            content = """
+                echo Get private signing key...
+                echo ${'$'}KEY_STORE > upload_key.hex
+                xxd -plain -revert upload_key.hex  upload_key.jks
+                echo Get Google service account key...
+                echo ${'$'}GOOGLE_SA_KEY > google_sa_key.hex
+                xxd -plain -revert google_sa_key.hex  google_sa_key.json
+                echo Build and publish AAB...
+                ./gradlew publishBundle
+            """
+        }
+    }
+}
 
 job("Test deployment") {
     host("Deploy test container") {
