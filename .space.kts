1b01b87541e499336d7df57356c9020f1268c287
amit.khetan.70 <amit.khetan.70@50gramx.io>
2023-12-15 20:13:32 +0530

    fix delivery

--- .space.kts
+++ .space.kts
@@ -180,6 +180,21 @@ job("Build and publish bundle to internal track") {
         }
     }
 
+    // To check a condition, basically, you need a kotlinScript step
+    host(displayName = "Setup Version") {
+        kotlinScript { api ->
+            // Get the current year and month
+            val currentYear = (LocalDate.now().year % 100).toString().padStart(2, '0')
+            val currentMonth = LocalDate.now().monthValue.toString()
+
+            // Get the execution number from environment variables
+            val currentExecution = System.getenv("JB_SPACE_EXECUTION_NUMBER")
+
+            // Set the VERSION_NUMBER parameter
+            api.parameters["VERSION_NUMBER"] = "$currentYear.$currentMonth.$currentExecution"
+        }
+    }
+
     container("Build and publish", "50gramx.registry.jetbrains.space/p/main/ethosindiacontainers/android-base:latest") {
         env["GOOGLE_SA_KEY"] = Secrets("GOOGLE_SERVICE_ACCOUNT_KEY")
         env["KEY_STORE"] = Secrets("PLAY_APP_SIGNING_UPLOAD_KEY")
