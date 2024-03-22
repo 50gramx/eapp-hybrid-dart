4ff039cdf7a2a76397b685bdb6d6900800467548
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-03-22 16:15:16 +0530

    fix version

--- .space.kts
+++ .space.kts
@@ -164,7 +164,7 @@ job("Build and publish bundle to web track") {
             api.space().projects.automation.deployments.start(
                     project = api.projectIdentifier(),
                     targetIdentifier = TargetIdentifier.Key("evo-on-50gramx-com"),
-                    version = "${System.getenv("VERSION_NUMBER")}",
+                    version = api.parameters["VERSION_NUMBER"],
                     // automatically update deployment status based on a status of a job
                     syncWithAutomationJob = true
             )
