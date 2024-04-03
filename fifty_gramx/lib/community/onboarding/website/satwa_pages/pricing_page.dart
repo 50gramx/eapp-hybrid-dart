88f5c22deaa1237fed83bf518da3ece4e60991e2
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-04-03 10:19:19 +0530

    fix pricing page satwa

---
+++ fifty_gramx/lib/community/onboarding/website/satwa_pages/pricing_page.dart
@@ -0,0 +1,66 @@
+/*
+ * ************************************************************************
+ *
+ *  AMIT KUMAR KHETAN CONFIDENTIAL
+ *  __________________
+ *
+ *   [2017] - [2022] Amit Kumar Khetan
+ *   All Rights Reserved.
+ *
+ *  NOTICE:  All information contained herein is, and remains
+ *  the property of Amit Kumar Khetan and its suppliers,
+ *  if any.  The intellectual and technical concepts contained
+ *  herein are proprietary to Amit Kumar Khetan
+ *  and its suppliers and may be covered by U.S. and Foreign Patents,
+ *  patents in process, and are protected by trade secret or copyright law.
+ *  Dissemination of this information or reproduction of this material
+ *  is strictly forbidden unless prior written permission is obtained
+ *  from Amit Kumar Khetan.
+ * /
+ */
+
+import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/colors/AppColors.dart';
+import 'package:fifty_gramx/community/onboarding/website/satwa_pages/overview_page_sliver_body.dart';
+import 'package:fifty_gramx/ui/base_widget.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+/// This is the stateful widget that the main application instantiates.
+class SatwaPricingPage extends StatefulWidget {
+  const SatwaPricingPage({Key? key}) : super(key: key);
+
+  @override
+  State<SatwaPricingPage> createState() => _SatwaPricingPageState();
+}
+
+class _SatwaPricingPageState extends State<SatwaPricingPage> {
+  @override
+  void initState() {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: AppColors.backgroundPrimary(context),
+      statusBarBrightness: NeumorphicTheme.isUsingDark(context)
+          ? Brightness.light
+          : Brightness.dark,
+      statusBarIconBrightness: NeumorphicTheme.isUsingDark(context)
+          ? Brightness.light
+          : Brightness.dark,
+    ));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseWidget(
+      builder: (context, sizingInformation) {
+        return Scaffold(
+            backgroundColor: AppColors.backgroundSecondary(context),
+            body: buildSatwaOverviewPageSliverBody());
+      },
+    );
+  }
+}
