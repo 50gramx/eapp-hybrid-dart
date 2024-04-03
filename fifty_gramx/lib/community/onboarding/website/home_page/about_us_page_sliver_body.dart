5ab1825bbb87051350f77b3c26662c8e5a5db331
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-04-03 10:07:54 +0530

    fix format

--- fifty_gramx/lib/community/onboarding/website/home_page/about_us_page_sliver_body.dart
+++ fifty_gramx/lib/community/onboarding/website/home_page/about_us_page_sliver_body.dart
@@ -1,6 +1,4 @@
-import 'package:fifty_gramx/community/onboarding/website/galaxy_pages/app_bar.dart';
 import 'package:fifty_gramx/community/onboarding/website/home_page/app_bar.dart';
-import 'package:fifty_gramx/community/onboarding/website/home_page/value_prop_section.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 CustomScrollView buildHomeAboutUsPageSliverBody() {
