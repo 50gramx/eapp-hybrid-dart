f8c0b56f4ec82db6dc7dde5450cd0b34b53f1ea9
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-04-06 05:59:39 +0530

    fix get domain button

--- fifty_gramx/lib/community/onboarding/website/home_page/app_bar.dart
+++ fifty_gramx/lib/community/onboarding/website/home_page/app_bar.dart
@@ -41,13 +41,100 @@ class _HomePageSliverAppBarState extends State<HomePageSliverAppBar> {
       child: titleText,
     );
     var title = Hero(
-        tag: "",
+        tag: "50gramx-title-app-bar-hero",
         transitionOnUserGestures: true,
         child: Align(
           alignment: Alignment.centerLeft,
           child: tappableTitleText,
         ));
 
+    Widget tappableHubTitleContainer = Padding(
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+      child: NeumorphicButton(
+          provideHapticFeedback: true,
+          onPressed: () {
+            Navigator.pushNamed(context, '/domains');
+          },
+          style: NeumorphicStyle(
+            lightSource: NeumorphicTheme.isUsingDark(context)
+                ? LightSource.bottomRight
+                : LightSource.topLeft,
+            shadowLightColor: NeumorphicTheme.isUsingDark(context)
+                ? AppColors.gray600
+                : AppColors.backgroundSecondary(context),
+            shape: NeumorphicShape.flat,
+            boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(24)),
+            color: AppColors.contentPrimary(context),
+            border: NeumorphicBorder(
+              isEnabled: true,
+              color: AppColors.backgroundSecondary(context),
+              width: 2,
+            ),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          margin: const EdgeInsets.fromLTRB(16, 12, 16, 8),
+          child: Text(
+            "Get Domains",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: AppColors.contentInversePrimary(context),
+                fontSize: 16,
+                fontFamily: "Montserrat",
+                fontWeight: FontWeight.w500),
+          )),
+    );
+    var hubTitle = Hero(
+      tag: "50gramx-hub-title-app-bar-hero",
+      transitionOnUserGestures: true,
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: tappableHubTitleContainer,
+      ),
+    );
+
+    var tappableEthosverseTitle = GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          '/ethosverse',
+        );
+      },
+      child: Padding(
+          padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+          child: Container(
+            padding: EdgeInsets.only(left: 6, right: 6),
+            alignment: Alignment.center,
+            child: NeumorphicText(
+              "Ethosverse",
+              style: NeumorphicStyle(
+                lightSource: NeumorphicTheme.isUsingDark(context)
+                    ? LightSource.bottomRight
+                    : LightSource.topLeft,
+                shadowLightColor: NeumorphicTheme.isUsingDark(context)
+                    ? AppColors.gray600
+                    : AppColors.backgroundSecondary(context),
+                shape: NeumorphicShape.flat,
+                boxShape: NeumorphicBoxShape.roundRect(BorderRadius.only(
+                    topRight: Radius.circular(24),
+                    bottomRight: Radius.circular(24))),
+                color: AppColors.contentPrimary(context),
+                depth: 2,
+                disableDepth: false,
+                border: NeumorphicBorder(
+                  isEnabled: true,
+                  color: AppColors.backgroundInverseTertiary(context),
+                  width: 0.3,
+                ),
+              ),
+              textAlign: TextAlign.left,
+              textStyle: NeumorphicTextStyle(
+                  fontSize: 20,
+                  fontFamily: "Montserrat",
+                  fontWeight: FontWeight.w600,
+                  height: 1.25),
+            ),
+          )),
+    );
     var galaxyPopupMenuButton =
         GalaxyPageHeaderConfig().buildPopupMenuButton(context);
     var spacePopupMenuButton =
@@ -56,52 +143,7 @@ class _HomePageSliverAppBarState extends State<HomePageSliverAppBar> {
         HomePageHeaderConfig().buildPopupMenuButton(context);
 
     var leftNavigationActions = [
-      GestureDetector(
-        onTap: () {
-          Navigator.pushNamed(
-            context,
-            '/ethosverse',
-          );
-        },
-        child: Padding(
-            padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
-            child: Container(
-              padding: EdgeInsets.only(left: 6, right: 6),
-              alignment: Alignment.center,
-              child: NeumorphicText(
-                "Ethosverse",
-                style: NeumorphicStyle(
-                  lightSource: NeumorphicTheme.isUsingDark(context)
-                      ? LightSource.bottomRight
-                      : LightSource.topLeft,
-                  shadowLightColor: NeumorphicTheme.isUsingDark(context)
-                      ? AppColors.gray600
-                      : AppColors.backgroundSecondary(context),
-                  shape: NeumorphicShape.flat,
-                  boxShape: NeumorphicBoxShape.roundRect(BorderRadius.only(
-                      topRight: Radius.circular(24),
-                      bottomRight: Radius.circular(24))),
-                  color: AppColors.contentPrimary(context),
-                  depth: 2,
-                  disableDepth: false,
-                  border: NeumorphicBorder(
-                    isEnabled: true,
-                    color: AppColors.backgroundInverseTertiary(context),
-                    width: 0.3,
-                  ),
-                ),
-                textAlign: TextAlign.left,
-                textStyle: NeumorphicTextStyle(
-                    fontSize: 20,
-                    fontFamily: "Montserrat",
-                    fontWeight: FontWeight.w600,
-                    height: 1.25),
-              ),
-            )),
-      ),
-      galaxyPopupMenuButton,
-      spacePopupMenuButton,
-      companyPopuMenuButton,
+      hubTitle,
     ];
 
     List<Widget> bottomNavigationActions = [
@@ -116,7 +158,17 @@ class _HomePageSliverAppBarState extends State<HomePageSliverAppBar> {
     bool isNavigatingLeft = LayoutBreakpoint().isNavigatingLeft(context);
 
     return SliverAppBar(
-      title: title,
+      title: Row(
+        children: [
+          // Add space to separate the title and other actions
+          title, // Your existing title widget
+          SizedBox(width: 16),
+          tappableEthosverseTitle,
+          galaxyPopupMenuButton,
+          spacePopupMenuButton,
+          companyPopuMenuButton,
+        ],
+      ),
       automaticallyImplyLeading: false,
       backgroundColor: AppColors.backgroundPrimary(context),
       pinned: widget.isPinned,
