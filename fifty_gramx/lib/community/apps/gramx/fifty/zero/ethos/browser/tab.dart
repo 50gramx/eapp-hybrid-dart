005bfbfad65cb8f004a077b8db4f1a324f241b01
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-06-13 16:17:02 +0530

    fix reload of page with setstate

--- fifty_gramx/lib/community/apps/gramx/fifty/zero/ethos/browser/tab.dart
+++ fifty_gramx/lib/community/apps/gramx/fifty/zero/ethos/browser/tab.dart
@@ -3,6 +3,7 @@ import 'package:fifty_gramx/services/notification/notifications_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'dart:ui' as ui;
+import 'dart:ui_web' as ui_web;
 
 class BrowserTab extends StatefulWidget {
   final String url;
@@ -21,183 +22,48 @@ class _BrowserTabState extends State<BrowserTab> {
 
   @override
   void initState() {
+    super.initState();
     url = widget.url;
-    // Listen for local notification for changes to underlying service
+    iframe = createIFrame(url);
+
+    // Listen for local notification for changes to the underlying service
     _notificationsStream.listen((event) {
       if (event.type == "LocalAssistantConversation") {
         if (event.data["subType"] == "validatedURL") {
           print("Browser tab: ${event.data["url"]}");
           setState(() {
             url = event.data["url"];
+            iframe = createIFrame(url);
+            registerIFrameView(iframe, url);
           });
         }
-      }});
-    super.initState();
+      }
+    });
 
-    iframe = html.IFrameElement()
-      ..src = url
-      ..style.border = 'none'
-      ..width = '100%'
-      ..height = '100%'
-      ..attributes.remove('sandbox');  // Allow JavaScript
+    registerIFrameView(iframe, url);
+  }
 
-    // Append the iframe to a container div
-    final divElement = html.DivElement()
-      ..style.width = '100%'
-      ..style.height = '100vh' // Adjust height as needed
-      ..style.overflow = 'auto' // To allow scrolling
-      ..style.border = '1px solid #ccc'
-      ..append(iframe);
-
-    // Register the view type for the iframe
+  void registerIFrameView(html.IFrameElement iframe, String url) {
     final iframeId = 'iframe-$url';
     if (kIsWeb) {
-      ui.platformViewRegistry.registerViewFactory(iframeId, (int viewId) => divElement);
+      ui_web.platformViewRegistry
+          .registerViewFactory(iframeId, (int viewId) => iframe);
     }
+  }
+
+  html.IFrameElement createIFrame(String url) {
+    return html.IFrameElement()
+      ..src = url
+      ..style.border = 'none'
+      ..style.height = '100%'
+      ..style.width = '100%'
+      ..attributes.remove('sandbox'); // Allow JavaScript
   }
 
   @override
   Widget build(BuildContext context) {
     return kIsWeb
-        ? HtmlElementView(viewType: 'iframe-$url')
+        ? HtmlElementView(key: UniqueKey(), viewType: 'iframe-$url')
         : Center(child: Text('This feature is only available on the web.'));
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'dart:html' as html;
-//
-// class BrowserTab extends StatefulWidget{
-//   final String url;
-//
-//   const BrowserTab({Key? key, required this.url}) : super(key: key);
-//
-//   @override
-//   State<BrowserTab> createState() => _BrowserTabState();
-// }
-//
-// class _BrowserTabState extends State<BrowserTab> {
-//
-//   late String url;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Replace 'https://www.example.com' with your desired URL
-//     // String url = 'https://www.50gramx.com';
-//     url = widget.url;
-//     url = 'https://www.pitchfork.com';
-//     html.IFrameElement iframe = html.IFrameElement();
-//     iframe.src = url;
-//     iframe.style.border = 'none';
-//     iframe.width = '100%';
-//     iframe.height = '100%';
-//     // Allow JavaScript
-//     iframe.attributes.remove('sandbox');
-//     html.DivElement divElement = html.DivElement()
-//       ..style.width = '100%'
-//       ..style.height = '100vh' // adjust height as needed
-//       ..style.overflow = 'auto' // to allow scrolling
-//       ..style.border = '1px solid #ccc'
-//       ..append(iframe);
-//     html.DivElement wrapper = html.DivElement()
-//       ..append(divElement);
-//     html.Element body = html.document.body!;
-//     body.children.add(wrapper);
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//     );
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
-  // final List<String> _tabs = ['https://www.google.com'];
-  // final TextEditingController _urlController = TextEditingController();
-  //
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: TextField(
-  //         controller: _urlController,
-  //         onSubmitted: (url) {
-  //           _launchUrl(url);
-  //         },
-  //         decoration: InputDecoration(
-  //           hintText: 'Enter URL',
-  //           suffixIcon: IconButton(
-  //             icon: Icon(Icons.search),
-  //             onPressed: () {
-  //               _launchUrl(_urlController.text);
-  //             },
-  //           ),
-  //         ),
-  //       ),
-  //     ),
-  //     body: ListView.builder(
-  //       itemCount: _tabs.length,
-  //       itemBuilder: (context, index) {
-  //         return ListTile(
-  //           title: Text(_tabs[index]),
-  //           onTap: () {
-  //             _launchUrl(_tabs[index]);
-  //           },
-  //         );
-  //       },
-  //     ),
-  //     floatingActionButton: FloatingActionButton(
-  //       onPressed: addTab,
-  //       child: Icon(Icons.add),
-  //     ),
-  //   );
-  // }
-  //
-  // _launchUrl(String url) async {
-  //   Uri uri = Uri.parse(url);
-  //   if (await canLaunchUrl(uri)) {
-  //     await launchUrl(uri, mode: LaunchMode.inAppWebView);
-  //   } else {
-  //     throw 'Could not launch $url';
-  //   }
-  // }
-  //
-  // void addTab() {
-  //   setState(() {
-  //     _tabs.add('https://www.google.com');
-  //   });
-  // }
-
