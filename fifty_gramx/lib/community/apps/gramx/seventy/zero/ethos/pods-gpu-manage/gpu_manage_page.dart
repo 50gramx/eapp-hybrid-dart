39b594b8a1d4c7c561ad89b15de5b2a6e5478073
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-07-05 19:25:59 +0530

    fixed button

--- fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods-gpu-manage/gpu_manage_page.dart
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods-gpu-manage/gpu_manage_page.dart
@@ -1,3 +1,5 @@
+import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/eutopia/ethosapps/eapp_flow_bob.dart';
+import 'package:fifty_gramx/services/notification/notifications_bloc.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -46,7 +48,6 @@ class _PodListPageState extends State<PodListPage> {
       print("PodListPage: response body: ${response.body}");
 
       if (response.statusCode == 200) {
-        // Parse the 'pods' array directly from the JSON response
         List<dynamic> podsJson = json.decode(response.body)['pods'];
         List<Pod> pods = podsJson.map((json) => Pod.fromJson(json)).toList();
         return pods;
@@ -93,18 +94,74 @@ class _PodListPageState extends State<PodListPage> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          pod.name,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  pod.name,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  pod.namespace,
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  pod.status,
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: pod.status == 'Running'
+                                        ? Colors.green
+                                        : Colors.red,
+                                  ),
+                                ),
+                                Switch(
+                                  value: pod.status == 'Running',
+                                  onChanged: (value) {},
+                                  activeColor: Colors.green,
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                        SizedBox(height: 5),
-                        Text('Namespace: ${pod.namespace}'),
-                        Text('Node Name: ${pod.nodeName}'),
-                        Text('SSH Command: ${pod.sshCommand}'),
-                        Text('Status: ${pod.status}'),
+                        SizedBox(height: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                // Add your deployment logic here
+                                EthosAppFlowBob().loadAppOnTheGo(
+                                    appName: "pods-gpu-cli",
+                                    orgName: "ethos",
+                                    communityCode: 70,
+                                    appIndex: 70093);
+                                NotificationsBloc.instance.newNotification(
+                                    LocalNotification("EthosAppFlowBob", {
+                                  "subType": "Open eApp",
+                                  "appSectionIndex": 5
+                                }));
+                              },
+                              child: Text(
+                                pod.sshCommand,
+                                style: TextStyle(color: Colors.blue),
+                              ),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
                   ),
