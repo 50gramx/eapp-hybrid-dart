a1482e9991b1516f082ad576cb52448a0a0f5aa3
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-08-18 08:37:57 +0530

    fixed drawer

--- fifty_gramx/lib/community/onboarding/website/home_page/app_drawer.dart
+++ fifty_gramx/lib/community/onboarding/website/home_page/app_drawer.dart
@@ -1,11 +1,43 @@
 import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/colors/AppColors.dart';
+import 'package:fifty_gramx/community/onboarding/website/galaxy_pages/header_config.dart';
+import 'package:fifty_gramx/community/onboarding/website/home_page/app_bar.dart';
+import 'package:fifty_gramx/community/onboarding/website/home_page/header_config.dart';
+import 'package:fifty_gramx/community/onboarding/website/space_pages/header_config.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 
 Widget buildHomePageAppDrawer(BuildContext context) {
   return Container(
     height: MediaQuery.of(context).size.height,
     width: MediaQuery.of(context).size.width,
-    color: AppColors.ErrorColor,
-    child: SizedBox(),
+    color: AppColors.backgroundSecondary(context),
+    child: Stack(
+      children: [
+        ListView(
+          padding: EdgeInsets.only(top: 48, left: 16, right: 16, bottom: 48),
+          children: <Widget>[
+            GalaxyPageHeaderConfig().buildCollapsibleMenu(context),
+            SpacePageHeaderConfig().buildCollapsibleMenu(context),
+            HomePageHeaderConfig().buildCollapsibleMenu(context),
+            // Add more items here as needed
+          ],
+        ),
+        Positioned(
+          top: 8,
+          left: 16,
+          child: buildHerotitle(context),
+        ),
+        Positioned(
+          top: 8,
+          right: 4,
+          child: IconButton(
+            icon: Icon(FeatherIcons.x),
+            onPressed: () {
+              Navigator.pop(context); // Closes the drawer
+            },
+          ),
+        ),
+      ],
+    ),
   );
 }
