a2af12d681a8f658c359a620e6c067379b7a1c9e
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-08-19 17:28:49 +0530

    fixed about us page

--- fifty_gramx/lib/community/onboarding/website/home_page/about_us_page_sliver_body.dart
+++ fifty_gramx/lib/community/onboarding/website/home_page/about_us_page_sliver_body.dart
@@ -1,3 +1,4 @@
+import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/colors/AppColors.dart';
 import 'package:fifty_gramx/community/onboarding/website/home_page/app_bar.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
@@ -12,40 +13,64 @@ CustomScrollView buildHomeAboutUsPageSliverBody() {
           itemBuilder: (BuildContext context, int position,
               Animation<double> animation) {
             if (position == 0) {
-              return _buildSection('About Company', '''
-50GRAMx is a decentralized computing network that aims to create a community-driven and owned platform for processing data and knowledge sharing. The project was founded in 2020 by a single individual with the vision of creating a more resilient and community-driven internet and is currently in the early stages of development.
+              return _buildSection(
+                  'About Us',
+                  '''
+Welcome to 50GRAMx!
 
-The name 50GRAMx is derived from three components: "50", "GRAM", and "x".
+At 50GRAMx, we're redefining how data is processed and knowledge is shared. Our decentralized computing network brings together communities to build a more resilient and community-driven internet. Founded in 2020 with a vision for a better digital world, our platform empowers individuals and communities to collaborate beyond traditional barriers.
 
-- The number 50 represents the maximum number of communities that can be part of a given Ethosverse, which is a collection of galaxies that share a common ethos and vision. Of the 50 communities, 49 will be open communities, and one will be a classical community that acts as a gateway to the wider internet.
+Our Name:
+- 50: Represents the maximum number of communities within an Ethosverse, with 49 open and one classical community acting as a gateway.
+- GRAM: Symbolizes the weight of collective knowledge.
+- x: Signifies limitless potential and growth.
 
-- The word "GRAM" represents the weight of a community's collective knowledge and understanding,
-
-- While the letter "x" represents the potential for limitless growth and evolution.
-
-50GRAMx aims to create a decentralized network that empowers communities to collaborate and share knowledge without relying on centralized institutions. Each 50GRAMx Galaxy represents the collective processing power of a community, with each individual's knowledge contributing to the strength and resilience of the network as a whole. As more Galaxies join the network, the collective processing power and knowledge base of the community will grow, making it more robust and resistant to attacks.
-
-The 50GRAMx network is built on a distributed computing infrastructure that allows community members to contribute their computing resources to the network in exchange for rewards in EthosCoin, the native cryptocurrency of the platform. These resources can be used to process data and run decentralized applications, allowing community members to earn rewards while contributing to the growth of the network.
-
-50GRAMx is still in the early stages of development, with the team working on building out the infrastructure and onboarding new community members. The project aims to create a decentralized knowledge and processing power network that can help drive innovation and collaboration across diverse communities.
-          ''');
+Our platform is designed to leverage distributed computing to enhance collaboration and innovation. With each new Galaxy joining, we amplify our network’s strength and resilience.
+              ''',
+                  context);
             } else if (position == 1) {
-              return _buildSection('Mission',
-                  'Our mission is to create a decentralized computing network that empowers communities to collaborate and share knowledge without relying on centralized institutions.');
+              return _buildSection(
+                  'Our Mission',
+                  '''
+Empowering Communities Through Decentralization
+
+Our mission is to foster a collaborative environment where communities can share and process data without the constraints of centralized institutions. We believe in the power of decentralized networks to drive meaningful change and innovation.
+              ''',
+                  context);
             } else if (position == 2) {
-              return _buildSection('Vision',
-                  'Our vision is to build a resilient and community-driven internet where every individual has the opportunity to contribute to and benefit from the collective knowledge of humanity.');
+              return _buildSection(
+                  'Our Vision',
+                  '''
+Building a Resilient and Inclusive Digital Future
+
+We envision an internet where every individual can contribute to and benefit from the collective knowledge of humanity. Our goal is to create a resilient, community-driven network that thrives on shared insights and collective effort.
+              ''',
+                  context);
             } else if (position == 3) {
-              return _buildSection('Office Address', """Floor No.: 4th FLOOR
-Building No./Flat No.: UNIT FO-02
-Road/Street: 28/A 80 FEET RD
-Locality/Sub Locality: Indiranagar Bangalore
-City/Town/Village: Bengaluru
-District: Bengaluru Urban
-State: Karnataka
-PIN Code: 560038""");
+              return _buildSection(
+                  'Office Address',
+                  '''
+Our Headquarters:
+
+- Floor No.: 4th FLOOR
+- Building No./Flat No.: UNIT FO-02
+- Road/Street: 28/A 80 FEET RD
+- Locality/Sub Locality: Indiranagar Bangalore
+- City/Town/Village: Bengaluru
+- District: Bengaluru Urban
+- State: Karnataka
+- PIN Code: 560038
+              ''',
+                  context);
             } else if (position == 4) {
-              return _buildSection('GST Number', '29AAFCF7727D1ZI');
+              return _buildSection(
+                  'GST Number',
+                  '''
+Tax Information:
+
+- GST Number: 29AAFCF7727D1ZI
+              ''',
+                  context);
             } else {
               return SizedBox();
             }
@@ -54,23 +79,41 @@ PIN Code: 560038""");
   );
 }
 
-Widget _buildSection(String title, String content) {
+Widget _buildSection(String title, String content, BuildContext context) {
   return Container(
+    padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
     margin: EdgeInsets.only(bottom: 16.0),
+    decoration: BoxDecoration(
+      color: AppColors.backgroundPrimary(context),
+      borderRadius: BorderRadius.circular(12.0),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.1),
+          blurRadius: 8.0,
+          offset: Offset(0, 4),
+        ),
+      ],
+    ),
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           title,
           style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
+            fontSize: 22.0,
+            fontWeight: FontWeight.w600,
+            fontFamily: 'Montserrat',
+            color: AppColors.contentPrimary(context),
           ),
         ),
-        SizedBox(height: 8.0),
+        SizedBox(height: 12.0),
         Text(
           content,
-          style: TextStyle(fontSize: 16.0),
+          style: TextStyle(
+            fontSize: 16.0,
+            fontFamily: 'Montserrat',
+            color: AppColors.contentPrimary(context).withOpacity(0.8),
+          ),
         ),
       ],
     ),
