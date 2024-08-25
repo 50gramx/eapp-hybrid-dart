08838b891d515550a698d9566be584de1cf96d68
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-08-25 12:06:20 +0530

    fixed multipass install

--- fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/brew/install/brewInstallCommands.dart
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/brew/install/brewInstallCommands.dart
@@ -1,6 +1,7 @@
 import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/executer/privilegedCommandExecuter.dart';
 import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/executer/simpleCommandExecuter.dart';
 import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/microk8s/microk8sCommands.dart';
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/multipass/multipassCommands.dart';
 import 'package:fifty_gramx/data/hostUserData.dart';
 
 class BrewInstallCommands {
@@ -23,27 +24,31 @@ class BrewInstallCommands {
   }
 
   multipass() async {
-    // build the command
-    String command = "${_baseCommandSpace}"
-        "--cask multipass";
-    // get the host user password
-    String password = await HostUserData().readHostUserPassword();
-    String assetPath =
-        "lib/community/apps/gramx/seventy/zero/ethos/pods/configurations/orchestrator/setup/scripts/installMultipass.sh";
-    var installPath =
-        await PrivilegedCommandExecuter.getTemporaryFilePathForAssetFile(
-            assetPath);
-    // Update the spawn command in install script
-    await SimpleCommandExecuter.run(
-        '''sed -e '4s#.*#spawn $command#' -i '' $installPath''');
-    // Update password in install script
-    await SimpleCommandExecuter.run(
-        '''sed -e '6s/.*/send "$password"/' -i '' $installPath''');
-    // Run the install script
-    print("will run installer");
-    await SimpleCommandExecuter.run("/bin/bash $installPath");
-    // Wait for the socket to up and running
-    print("will start waiting for 10sec");
-    await Future.delayed(Duration(seconds: 10));
+    if (!await MultipassCommands.version.isPresent()) {
+      // build the command
+      String command = "${_baseCommandSpace}"
+          "multipass";
+      // get the host user password
+      String password = await HostUserData().readHostUserPassword();
+      String assetPath =
+          "lib/community/apps/gramx/seventy/zero/ethos/pods/configurations/orchestrator/setup/scripts/installMultipass.sh";
+      var installPath =
+          await PrivilegedCommandExecuter.getTemporaryFilePathForAssetFile(
+              assetPath);
+      // Update the spawn command in install script
+      print("Update the spawn command in install script");
+      await SimpleCommandExecuter.run(
+          '''sed -e '4s#.*#spawn $command#' -i '' $installPath''');
+      // Update password in install script
+      print("Update password in install script");
+      await SimpleCommandExecuter.run(
+          '''sed -e '6s/.*/send "$password"/' -i '' $installPath''');
+      // Run the install script
+      print("will run installer");
+      await SimpleCommandExecuter.run("/bin/bash $installPath");
+      // Wait for the socket to up and running
+      print("will start waiting for 10sec");
+      await Future.delayed(Duration(seconds: 20));
+    }
   }
 }
