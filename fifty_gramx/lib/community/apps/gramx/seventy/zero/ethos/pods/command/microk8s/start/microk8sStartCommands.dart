86636dd193f5d97f747e92345c0db07cdba9f8ad
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-08-26 01:10:55 +0530

    added microk8s start commands

---
+++ fifty_gramx/lib/community/apps/gramx/seventy/zero/ethos/pods/command/microk8s/start/microk8sStartCommands.dart
@@ -0,0 +1,34 @@
+// operates the status commands for microk8s package
+import 'dart:convert';
+
+import 'package:fifty_gramx/community/apps/gramx/seventy/zero/ethos/pods/command/executer/simpleCommandExecuter.dart';
+import 'package:process_run/shell.dart';
+
+class Microk8sStartCommands {
+  // singleton definition
+  Microk8sStartCommands._();
+
+  /// contains the package path
+  static String _packagePath = "";
+
+  /// contains the base command for status commands
+  static String _baseCommandSpace = "";
+
+  // initialises the class
+  Microk8sStartCommands(String packagePath) {
+    _packagePath = packagePath;
+    _baseCommandSpace = "${_packagePath}start ";
+  }
+
+  Future<bool> now() async {
+    String command = "$_baseCommandSpace";
+    print("microk8s starting now");
+    String output = (await SimpleCommandExecuter.run(command)).outText;
+    var firstLineOP = LineSplitter.split(output).first;
+    if (firstLineOP == "Started.") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
