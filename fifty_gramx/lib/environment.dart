489d66365687e11086a02c4e1bcfaeb6fd432e42
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-08-28 04:04:56 +0530

    refactored envs

--- fifty_gramx/lib/environment.dart
+++ fifty_gramx/lib/environment.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 class Environment {
