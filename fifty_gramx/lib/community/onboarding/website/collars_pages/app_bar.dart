5c677aaae465b3513e0f342570b622d8b7c30025
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-08-31 11:46:46 +0530

    fixed collar page appbar

--- fifty_gramx/lib/community/onboarding/website/collars_pages/app_bar.dart
+++ fifty_gramx/lib/community/onboarding/website/collars_pages/app_bar.dart
@@ -1,4 +1,4 @@
-import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/colors/AppColors.dart';
+import 'package:fifty_gramx/community/onboarding/website/collars_pages/header_config.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class CollarsOverviewPageSliverAppBar extends StatefulWidget {
@@ -19,40 +19,6 @@ class _CollarsOverviewPageSliverAppBarState
     extends State<CollarsOverviewPageSliverAppBar> {
   @override
   Widget build(BuildContext context) {
-    Widget titleText = Text(
-      "Collars",
-      maxLines: 1,
-      textAlign: TextAlign.start,
-      style: TextStyle(
-          color: AppColors.contentPrimary(context),
-          fontSize: 24,
-          fontFamily: "Montserrat",
-          fontWeight: FontWeight.w500),
-    );
-    Widget tappableTitleText = GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(context, '/collars');
-      },
-      child: titleText,
-    );
-    var title = Hero(
-        tag: "50gramx-domains-title-app-bar-hero",
-        transitionOnUserGestures: true,
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: tappableTitleText,
-        ));
-
-    return SliverAppBar(
-      title: title,
-      automaticallyImplyLeading: false,
-      backgroundColor: AppColors.backgroundPrimary(context),
-      pinned: true,
-      stretch: true,
-      onStretchTrigger: () async {
-        print("OnStretchTrigger:start");
-        print("OnStretchTrigger:finish");
-      },
-    );
+    return CollarsPageHeaderConfig().buildSliverAppBar(context);
   }
 }
---
+++ fifty_gramx/lib/community/onboarding/website/collars_pages/header_config.dart
@@ -0,0 +1,11 @@
+import 'package:fifty_gramx/community/onboarding/website/page_header_config.dart';
+
+class CollarsPageHeaderConfig extends PageHeaderConfig {
+  CollarsPageHeaderConfig()
+      : super(
+          header: 'Collars',
+          items: [
+            {'title': 'Store', 'route': '/collars'},
+          ],
+        );
+}
