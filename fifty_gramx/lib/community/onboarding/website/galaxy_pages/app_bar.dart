a866135bbf205eb66c30170fcbe5bc7c11d725f8
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-08-31 11:48:44 +0530

    fixed galaxy appbar

--- fifty_gramx/lib/community/onboarding/website/galaxy_pages/app_bar.dart
+++ fifty_gramx/lib/community/onboarding/website/galaxy_pages/app_bar.dart
@@ -1,4 +1,3 @@
-import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/colors/AppColors.dart';
 import 'package:fifty_gramx/community/onboarding/website/galaxy_pages/header_config.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
@@ -20,44 +19,6 @@ class _GalaxyOverviewPageSliverAppBarState
     extends State<GalaxyOverviewPageSliverAppBar> {
   @override
   Widget build(BuildContext context) {
-    Widget titleText = Text(
-      "Galaxy",
-      maxLines: 1,
-      textAlign: TextAlign.start,
-      style: TextStyle(
-          color: AppColors.contentPrimary(context),
-          fontSize: 24,
-          fontFamily: "Montserrat",
-          fontWeight: FontWeight.w500),
-    );
-    Widget tappableTitleText = GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(context, '/galaxy');
-      },
-      child: titleText,
-    );
-    var title = Hero(
-        tag: "",
-        transitionOnUserGestures: true,
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: tappableTitleText,
-        ));
-
-    GalaxyPageHeaderConfig galaxyConfig = GalaxyPageHeaderConfig();
-    var actions = galaxyConfig.buildActions(context);
-
-    return SliverAppBar(
-      title: title,
-      automaticallyImplyLeading: false,
-      backgroundColor: AppColors.backgroundPrimary(context),
-      pinned: true,
-      actions: actions,
-      stretch: true,
-      onStretchTrigger: () async {
-        print("OnStretchTrigger:start");
-        print("OnStretchTrigger:finish");
-      },
-    );
+    return GalaxyPageHeaderConfig().buildSliverAppBar(context);
   }
 }
