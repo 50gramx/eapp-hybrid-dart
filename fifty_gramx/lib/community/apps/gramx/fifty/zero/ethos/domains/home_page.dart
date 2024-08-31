8c895e0d9f8dcefdf6d020196905b7ddeca42365
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-08-31 22:28:43 +0530

    fixed domain widget usage

--- fifty_gramx/lib/community/apps/gramx/fifty/zero/ethos/domains/home_page.dart
+++ fifty_gramx/lib/community/apps/gramx/fifty/zero/ethos/domains/home_page.dart
@@ -29,17 +29,19 @@ class _HomePageState extends State<HomePage> {
     ),
     SpaceKnowledgeDomain(
       spaceKnowledgeDomainName: "Sendil",
-      spaceKnowledgeDomainDescription: "Sendil is your dedicated digital real estate assistant, designed to streamline and simplify the property prospecting and onboarding process in Indiranagar, Bangalore. With Sendil, you'll experience hassle-free property search, inquiries, and visit scheduling, saving you and your clients valuable time.",
+      spaceKnowledgeDomainDescription:
+          "Sendil is your dedicated digital real estate assistant, designed to streamline and simplify the property prospecting and onboarding process in Indiranagar, Bangalore. With Sendil, you'll experience hassle-free property search, inquiries, and visit scheduling, saving you and your clients valuable time.",
     ),
     SpaceKnowledgeDomain(
       spaceKnowledgeDomainName: "Irani",
-      spaceKnowledgeDomainDescription: "Irani is your dedicated college admission advisor, specializing in helping aspiring engineering students find their dream colleges. With a friendly and empathetic approach, Irani 88 simplifies the complex task of matching JEE ranks with college cutoffs, ensuring you make informed decisions for your academic future.",
+      spaceKnowledgeDomainDescription:
+          "Irani is your dedicated college admission advisor, specializing in helping aspiring engineering students find their dream colleges. With a friendly and empathetic approach, Irani 88 simplifies the complex task of matching JEE ranks with college cutoffs, ensuring you make informed decisions for your academic future.",
     ),
     SpaceKnowledgeDomain(
       spaceKnowledgeDomainName: "Irani",
-      spaceKnowledgeDomainDescription: "Rashmi is your dedicated rental property assistant, specially designed to simplify the process of finding a place to rent near your office address in Indiranagar, Bangalore. Whether you're a newcomer to the city or a long-time resident looking for a convenient rental, Rashmi ensures you connect with the right agent who can assist you effectively.",
+      spaceKnowledgeDomainDescription:
+          "Rashmi is your dedicated rental property assistant, specially designed to simplify the process of finding a place to rent near your office address in Indiranagar, Bangalore. Whether you're a newcomer to the city or a long-time resident looking for a convenient rental, Rashmi ensures you connect with the right agent who can assist you effectively.",
     ),
-  
   ]; // Replace with fetched data if applicable
 
   @override
@@ -52,36 +54,55 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     bool isNavigatingLeft = LayoutBreakpoint().isNavigatingLeft(context);
-    var drawer = isNavigatingLeft ? null : Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              child: Text('Ethos Domains'),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-              ),
+    var drawer = isNavigatingLeft
+        ? null
+        : Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                DrawerHeader(
+                  child: Text('Ethos Domains'),
+                  decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                  ),
+                ),
+
+                // Add more ListTile widgets for additional links
+              ],
             ),
-            
-            // Add more ListTile widgets for additional links
-          ],
-        ),
-      );
+          );
 
     Widget domainList = ListView.builder(
-        itemCount: ethosKnowledgeDomains.length,
-        itemBuilder: (context, index) {
-          return EthosDomainWidget(
-              knowledgeDomain: ethosKnowledgeDomains[index]);
-        },
-      );
+      itemCount: ethosKnowledgeDomains.length,
+      itemBuilder: (context, index) {
+        return EthosDomainWidget(
+          knowledgeDomain: ethosKnowledgeDomains[index],
+          primaryCTAtitle: "",
+          secondaryCTAtitle: "",
+        );
+      },
+    );
     Widget expandedDomainList = Expanded(child: domainList);
-    Widget mainColumn = Flexible(child: Column(children: [expandedDomainList],), flex: 7,);
-    Widget expandedColumn = isNavigatingLeft ? Flexible(child: Column(children: [],), flex: 3,) : SizedBox();
-    Widget mainRow = Row(children: [
-      mainColumn,
-      expandedColumn,
-    ],);
+    Widget mainColumn = Flexible(
+      child: Column(
+        children: [expandedDomainList],
+      ),
+      flex: 7,
+    );
+    Widget expandedColumn = isNavigatingLeft
+        ? Flexible(
+            child: Column(
+              children: [],
+            ),
+            flex: 3,
+          )
+        : SizedBox();
+    Widget mainRow = Row(
+      children: [
+        mainColumn,
+        expandedColumn,
+      ],
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text('Ethos Domains'),
