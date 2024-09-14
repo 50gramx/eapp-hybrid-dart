393ccf06d1f0deeecb7a12c04e5c86b777098ed5
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-09-14 18:40:26 +0530

    refactored

--- fifty_gramx/lib/community/homeScreenWidgets/configurations/selectorConfigurationItem.dart
+++ fifty_gramx/lib/community/homeScreenWidgets/configurations/selectorConfigurationItem.dart
@@ -1,19 +1,20 @@
 import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/colors/AppColors.dart';
-import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/components/Container/NeuContainer.dart';
 import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/components/Text/Title/listItemDisabledText.dart';
 import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/components/Text/Title/listItemTitleText.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class SelectorConfigurationItem extends StatelessWidget {
-
   final String titleText;
   final String subtitleText;
   final VoidCallback selectorCallback;
 
-  const SelectorConfigurationItem({Key? key, required this.titleText, required this.subtitleText, required this.selectorCallback}) : super(key: key);
+  const SelectorConfigurationItem(
+      {Key? key,
+      required this.titleText,
+      required this.subtitleText,
+      required this.selectorCallback})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -35,30 +36,39 @@ class SelectorConfigurationItem extends StatelessWidget {
           width: 2,
         ),
       ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(child: ListItemTitleTextWidget(titleText: titleText, disableNeu: true, fontSize: 18, fontWeight: FontWeight.w500,)),
-            ListItemSubtitleTextWidget(subtitleText: subtitleText, disableNeu: true, fontSize: 18, fontWeight: FontWeight.w500,),
-            Padding(
-              padding: EdgeInsets.only(left: 8),
-              child: CircleAvatar(
-                radius: 12,
-                child: Icon(
-                  FeatherIcons.chevronRight,
-                  size: 20,
-                  color: AppColors.contentPrimary(context),
-                ),
-                backgroundColor: AppColors.backgroundPrimary(context),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+              child: ListItemTitleTextWidget(
+            titleText: titleText,
+            disableNeu: true,
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          )),
+          ListItemSubtitleTextWidget(
+            subtitleText: subtitleText,
+            disableNeu: true,
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 8),
+            child: CircleAvatar(
+              radius: 12,
+              child: Icon(
+                FeatherIcons.chevronRight,
+                size: 20,
+                color: AppColors.contentPrimary(context),
               ),
+              backgroundColor: AppColors.backgroundPrimary(context),
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
       onPressed: () {
         selectorCallback();
       },
-
     );
   }
-
 }
