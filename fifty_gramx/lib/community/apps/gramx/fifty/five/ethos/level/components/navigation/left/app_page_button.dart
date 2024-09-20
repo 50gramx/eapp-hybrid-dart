c804512ddcce9e2e85f2ef9cf60f7fc9f3c7be3b
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-09-20 18:02:49 +0530

    fixed app page button

--- fifty_gramx/lib/community/apps/gramx/fifty/five/ethos/level/components/navigation/left/app_page_button.dart
+++ fifty_gramx/lib/community/apps/gramx/fifty/five/ethos/level/components/navigation/left/app_page_button.dart
@@ -1,18 +1,64 @@
 import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/eutopia/ethosapps/eapp_flow_bob.dart';
 import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/colors/AppColors.dart';
-import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/components/NeuButton/actionNeuChip.dart';
+import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/components/navigation/left/EutopiaLeftNavigationScaffold.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
-Widget buildAppPageButton(BuildContext context, int subIndex,
-    Function(int) selectPressedSectionItem) {
-  return ActionNeuChip(
-    chipAvatarText:
-        "${EthosAppFlowBob.eutopiaNavigationBarSectionalItems[subIndex].leftNavigationBarSectionalItem.code}",
-    chipTitle:
-        "${EthosAppFlowBob.eutopiaNavigationBarSectionalItems[subIndex].leftNavigationBarSectionalItem.label}",
-    chipIconBackgroundColor: AppColors.contentPrimary(context),
-    buttonActionOnPressed: () {
-      selectPressedSectionItem(subIndex);
-    },
+Widget buildAppPageButton(
+    BuildContext context,
+    int subIndex,
+    Function(int) selectPressedSectionItem,
+    EutopiaLeftNavigationScaffold parentWidget) {
+  bool isSelected = (subIndex == parentWidget.selectedIndex);
+
+  return Neumorphic(
+    style: NeumorphicStyle(
+      border: NeumorphicBorder(
+        isEnabled: true,
+        color: AppColors.backgroundPrimary(context),
+        width: 1,
+      ),
+      boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(24)),
+    ),
+    padding: const EdgeInsets.all(2),
+    margin: const EdgeInsets.fromLTRB(6, 6, 6, 6),
+    child: NeumorphicButton(
+        provideHapticFeedback: true,
+        onPressed: () {
+          selectPressedSectionItem(subIndex);
+        },
+        style: NeumorphicStyle(
+          shape: NeumorphicShape.convex,
+          boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(24)),
+          color: isSelected
+              ? AppColors.backgroundInverseTertiary(context)
+              : AppColors.backgroundTertiary(context),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        child: Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 8),
+              child: RichText(
+                text: TextSpan(children: [
+                  TextSpan(
+                    text:
+                        "${EthosAppFlowBob.eutopiaNavigationBarSectionalItems[subIndex].leftNavigationBarSectionalItem.label}",
+                    style: TextStyle(
+                        color: isSelected
+                            ? AppColors.contentInversePrimary(context)
+                            : AppColors.contentSecondary(context),
+                        fontSize: isSelected ? 16 : 14,
+                        fontFamily: "Montserrat",
+                        fontWeight:
+                            isSelected ? FontWeight.w700 : FontWeight.w500),
+                  )
+                ]),
+              ),
+            ),
+            SizedBox(
+              width: 8,
+            ),
+          ],
+        )),
   );
 }
