1039b791f6367c3e78937f27d8de706cc424d490
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-09-20 18:04:03 +0530

    fixed with top usage

--- fifty_gramx/lib/community/apps/gramx/fifty/five/ethos/level/components/navigation/left/eait_1005.dart
+++ fifty_gramx/lib/community/apps/gramx/fifty/five/ethos/level/components/navigation/left/eait_1005.dart
@@ -4,35 +4,116 @@ import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/componen
 import 'package:fifty_gramx/community/apps/gramx/fifty/five/ethos/level/components/navigation/left/app_button.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
-class EAIT1005 extends StatelessWidget {
+class EAIT1005 extends StatefulWidget {
   final int navigationViewPort;
   final Function(int) selectPressedSectionItem;
   final EutopiaLeftNavigationScaffold navigationWidget;
 
-  EAIT1005(
-      {required this.navigationViewPort,
-      required this.selectPressedSectionItem,
-      required this.navigationWidget});
+  EAIT1005({
+    required this.navigationViewPort,
+    required this.selectPressedSectionItem,
+    required this.navigationWidget,
+  });
+
+  @override
+  _EAIT1005State createState() => _EAIT1005State();
+}
+
+class _EAIT1005State extends State<EAIT1005> {
+  final Map<int, int> usageCount = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _updateUsageCount();
+  }
+
+  void _updateUsageCount() {
+    // Update usage count for current items
+    for (int i = 0;
+        i < EthosAppFlowBob.eutopiaNavigationBarSectionalItems.length;
+        i++) {
+      if (!usageCount.containsKey(i)) {
+        usageCount[i] = 0;
+      }
+    }
+    // Remove counts for items that no longer exist
+    usageCount.removeWhere((key, value) =>
+        key >= EthosAppFlowBob.eutopiaNavigationBarSectionalItems.length);
+  }
 
   @override
   Widget build(BuildContext context) {
+    print("building EAIT1005");
+
+    // Update the usage count to reflect any changes in the items
+    _updateUsageCount();
+
+    double screenHeight = MediaQuery.of(context).size.height;
+    double itemHeight = 54; // Adjust as necessary
+    int maxVisibleItems = ((screenHeight * 0.7) / itemHeight).floor();
+    int itemCount = EthosAppFlowBob.eutopiaNavigationBarSectionalItems.length;
+    int visibleItemCount =
+        itemCount < maxVisibleItems ? itemCount : maxVisibleItems;
+    print("visibleItemCount: $visibleItemCount");
+
+    List<int> topUsedItems = getTopUsedItems(visibleItemCount);
+    print("topUsedItems: $topUsedItems");
+
+    Widget container = Container(
+      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      height: visibleItemCount * itemHeight,
+      child: Neumorphic(
+        style: NeumorphicStyle(
+          lightSource: NeumorphicTheme.isUsingDark(context)
+              ? LightSource.bottomRight
+              : LightSource.topLeft,
+          shadowLightColor: NeumorphicTheme.isUsingDark(context)
+              ? AppColors.gray600
+              : AppColors.backgroundSecondary(context),
+          shape: NeumorphicShape.concave,
+          depth: -6,
+          boxShape: NeumorphicBoxShape.roundRect(
+              BorderRadius.all(Radius.circular(24))),
+          color: AppColors.backgroundInverseSecondary(context),
+          border: NeumorphicBorder(
+            isEnabled: true,
+            color: AppColors.backgroundInverseTertiary(context),
+            width: 2,
+          ),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+        child: Column(
+          children: topUsedItems.map((int subIndex) {
+            return buildAppButton(
+              context,
+              subIndex,
+              _interceptedSelectPressedSectionItem,
+              widget.navigationWidget,
+            );
+          }).toList(),
+        ),
+      ),
+    );
+
     return Visibility(
-        visible: navigationViewPort == 4 ||
-            navigationViewPort == 5 ||
-            navigationViewPort == 6 ||
-            navigationViewPort == 7,
-        child: Container(
-          margin: EdgeInsets.only(left: 8, right: 8),
-          height:
-          EthosAppFlowBob.eutopiaNavigationBarSectionalItems.length * 57,
-          child: ListView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount:
-              EthosAppFlowBob.eutopiaNavigationBarSectionalItems.length,
-              itemBuilder: (BuildContext context, int subIndex) {
-                return buildAppButton(context, subIndex,
-                    selectPressedSectionItem, navigationWidget);
-              }),
-        ));
+      visible: widget.navigationViewPort >= 4 && widget.navigationViewPort <= 7,
+      child: container,
+    );
+  }
+
+  void _interceptedSelectPressedSectionItem(int index) {
+    setState(() {
+      usageCount[index] =
+          (usageCount[index] ?? 0) + 1; // Update the usage count
+    });
+    widget.selectPressedSectionItem(index); // Call the original function
+  }
+
+  List<int> getTopUsedItems(int limit) {
+    var sortedUsageCount = usageCount.entries.toList()
+      ..sort((a, b) => b.value.compareTo(a.value));
+    return sortedUsageCount.take(limit).map((entry) => entry.key).toList();
   }
 }
