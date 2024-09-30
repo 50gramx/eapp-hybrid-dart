cad074144e7b93d5a6a88dfefd5997fbd6aa01f3
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-09-30 20:16:27 +0530

    refactored and added todos

--- .space.kts
+++ .space.kts
@@ -153,6 +153,25 @@ job("Build and publish bundle to web track") {
         }
     }
 
+    container(displayName = "Release - Web - Site", image = "50gramx.registry.jetbrains.space/p/main/ethosindiacontainers/web-base:latest") {
+        env["FIREBASE_TOKEN"] = Secrets("FIREBASE_TOKEN")
+        env["PACKGAGES_READ_TOKEN"] = Secrets("ETHOS_APP_SERVICE_CONTRACTS_PACKGAGES_READ_TOKEN")
+
+        shellScript {
+            content = """
+          	pwd
+          	ls -l
+            npm -v
+           	npm install -g n 
+            n stable
+          cd fifty_gramx && flutter clean && dart pub token add https://dart.pkg.jetbrains.space/50gramx/p/main/dart-delivery/ --env-var=PACKGAGES_READ_TOKEN && flutter pub get && flutter pub cache repair && flutter build web --dart-define=flavor=50.ethos.site --release && firebase deploy --only hosting:default --token ${"$"}FIREBASE_TOKEN
+            # Retrieve commit messages using Git log command
+            # Output commit messages to console
+            echo "Commit Messages:"
+            git log -n 3 --format=%B
+          """
+        }
+    }
 
     host("Build - Web - Site - Image") {
 
@@ -178,26 +197,6 @@ job("Build and publish bundle to web track") {
 
         requirements {
             workerTags("windows-pool")
-        }
-    }
-
-    container(displayName = "Release - Web - Site", image = "50gramx.registry.jetbrains.space/p/main/ethosindiacontainers/web-base:latest") {
-        env["FIREBASE_TOKEN"] = Secrets("FIREBASE_TOKEN")
-        env["PACKGAGES_READ_TOKEN"] = Secrets("ETHOS_APP_SERVICE_CONTRACTS_PACKGAGES_READ_TOKEN")
-
-        shellScript {
-            content = """
-          	pwd
-          	ls -l
-            npm -v
-           	npm install -g n 
-            n stable
-          cd fifty_gramx && flutter clean && dart pub token add https://dart.pkg.jetbrains.space/50gramx/p/main/dart-delivery/ --env-var=PACKGAGES_READ_TOKEN && flutter pub get && flutter pub cache repair && flutter build web --dart-define=flavor=50.ethos.site --release && firebase deploy --only hosting:default --token ${"$"}FIREBASE_TOKEN
-            # Retrieve commit messages using Git log command
-            # Output commit messages to console
-            echo "Commit Messages:"
-            git log -n 3 --format=%B
-          """
         }
     }
 
