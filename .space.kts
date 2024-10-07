b222b830f0ad09e7d5c64f143d399d307b717473
Saikiran <sai@50gramx.io>
2024-10-07 18:07:30 +0530

    Added build arg

--- .space.kts
+++ .space.kts
@@ -194,7 +194,8 @@ job("Build and publish bundle to web track") {
 
         dockerBuildPush {
             file = "Dockerfile.web.release"
-            extraArgsForBuildCommand = listOf("--build-arg=PACKAGES_READ_TOKEN")
+            args["PACKAGES_READ_TOKEN"] = Secrets("ETHOS_APP_SERVICE_CONTRACTS_PACKGAGES_READ_TOKEN")
+            extraArgsForBuildCommand = listOf("--build-arg=PACKAGES_READ_TOKEN=$PACKAGES_READ_TOKEN")
 
            // image tags
             val dockerHubRepo = "docker.io/ethosindia/eapp-hybrid-web-site"
